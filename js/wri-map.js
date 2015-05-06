e59e24bfe18f4581c8380a9d1c14ef62404133f9
Ryan McCahan <ryan@mccahan.net>
2015-05-06 14:02:53 -0600

    Search functionality

--- js/wri-map.js
+++ js/wri-map.js
@@ -1,4 +1,18 @@
 (function($){
+  $.fn.makeAbsolute = function(rebase) {
+    return this.each(function() {
+        var el = $(this);
+        var pos = el.position();
+        el.css({ position: "absolute",
+            marginLeft: 0, marginTop: 0,
+            top: pos.top, right: 0 });
+        if (rebase)
+            el.remove().appendTo("body");
+    });
+  };
+}(jQuery));
+
+(function($){
 
   $.fn.wriMap = function(options) {
     var defaults = {
@@ -7,13 +21,17 @@
       menuOpenButton: "#menu-open-button",
 
       searchExpandButton: "#search-button",
+      searchCollapseButton: "#search-collapse-button",
+      searchBox: "#search-box",
+      leaveSearchOpenAfterSearch: false,
       zoomHomeButton: "#home-button",
       zoomInButton: "#zoom-in-button",
       zoomOutButton: "#zoom-out-button",
 
       onZoomHome: function(){},
       onZoomIn: function(){},
-      onZoomOut: function(){}
+      onZoomOut: function(){},
+      onSearch: function(data){}
     };
 
     var settings = $.extend({}, defaults, options);
@@ -48,6 +66,26 @@
     });
     $(settings.zoomOutButton).click(function(e){
       $.proxy(settings.onZoomOut, this)();
+    });
+
+    $($("#right-controls .button:not(#search-expandable)").get().reverse()).each(function(){ $(this).makeAbsolute(); });
+    $(settings.searchExpandButton).click(function(e){
+      $("#search-expandable").addClass('expanded');
+      setTimeout(function(){
+        $("#search-expandable input[type=text]").focus();
+      },525);
+    });
+
+    $(settings.searchCollapseButton).click(function(e){
+      $("#search-expandable").removeClass('expanded');
+    });
+
+    $("#search-expandable form").submit(function(e){
+      e.preventDefault();
+      $.proxy(settings.onSearch)($(this).find('input[type=text]').val());
+      if (settings.leaveSearchOpenAfterSearch === false) {
+        $("#search-expandable").removeClass('expanded');
+      }
     });
   }
 
