b566192139eba35a04c1b38191e2470504b591f0
Ryan McCahan <ryan@mccahan.net>
2015-05-06 11:45:50 -0600

    Callbacks for the zoom buttons

--- js/wri-map.js
+++ js/wri-map.js
@@ -4,19 +4,30 @@
     var defaults = {
       menuOverlay: "#menu-open-overlay",
       menuOpenClass: "menu-open",
-      menuOpenButton: "#menu-open-button"
+      menuOpenButton: "#menu-open-button",
+
+      searchExpandButton: "#search-button",
+      zoomHomeButton: "#home-button",
+      zoomInButton: "#zoom-in-button",
+      zoomOutButton: "#zoom-out-button",
+
+      onZoomHome: function(){},
+      onZoomIn: function(){},
+      onZoomOut: function(){}
     };
 
     var settings = $.extend({}, defaults, options);
 
     initRibbon(settings);
     initMainMenu(settings);
+    initRightButtons(settings);
   };
 
   function initRibbon(settings) {
     if ($("#ribbon").length > 0)
       $("body").addClass('has-ribbon');
   }
+
   function initMainMenu(settings) {
     $(settings.menuOverlay).click(function(e){
       e.preventDefault();
@@ -25,6 +36,18 @@
     $(settings.menuOpenButton).click(function(e){
       e.preventDefault();
       $("body").toggleClass(settings.menuOpenClass);
+    });
+  }
+
+  function initRightButtons(settings) {
+    $(settings.zoomHomeButton).click(function(e){
+      $.proxy(settings.onZoomHome, this)();
+    });
+    $(settings.zoomInButton).click(function(e){
+      $.proxy(settings.onZoomIn, this)();
+    });
+    $(settings.zoomOutButton).click(function(e){
+      $.proxy(settings.onZoomOut, this)();
     });
   }
 
