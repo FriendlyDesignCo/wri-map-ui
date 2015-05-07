0158cc632517441c27f620cf65ef3965f3aef8bc
Ryan McCahan <ryan@mccahan.net>
2015-05-07 02:10:45 -0600

    Basic map controls

--- js/wri-map.js
+++ js/wri-map.js
@@ -27,6 +27,8 @@
       zoomInButton: "#zoom-in-button",
       zoomOutButton: "#zoom-out-button",
 
+      mapControlsToggleButton: ".map-controls-toggle-button",
+
       onZoomHome: function(){},
       onZoomIn: function(){},
       onZoomOut: function(){},
@@ -38,6 +40,7 @@
     initRibbon(settings);
     initMainMenu(settings);
     initRightButtons(settings);
+    initMapControls(settings);
   };
 
   function initRibbon(settings) {
@@ -84,6 +87,40 @@
       if ($.proxy(settings.onSearch)($(this).find('input[type=text]').val())) {
         $("#search-expandable").removeClass('expanded');
       }
+    });
+  }
+
+  function initMapControls(settings) {
+    $(settings.mapControlsToggleButton).click(function(e){
+      $("body").toggleClass('map-controls-open');
+      $("#map-control-container").css({'max-height':''}).removeClass('second-page');
+    });
+
+    $("#map-control-container").find('ul > li').each(function(){
+      $(this).append('<div class="clearfix"></div>');
+      if ($(this).find('ul').length > 0)
+        $(this).addClass('has-children');
+    });
+
+    $("#map-control-container li.has-children").click(function(){
+      if ($("#map-control-container > ul").length > 1)
+        $("#map-control-container > ul:last").remove();
+      $("#map-control-secondary-header").remove();
+
+      // Menu header
+      $("#map-control-header").append('<div id="map-control-secondary-header"><i class="fa fa-chevron-left"></i><span></span></div>');
+      $("#map-control-secondary-header").find('span').html($(this).find('span').html());
+      $("#map-control-header").addClass('second-page');
+
+      // Menu items
+      $("#map-control-container").append($(this).find('ul').clone().addClass('second-container'));
+      $("#map-control-container").addClass('second-page').css({'max-height':$('ul.second-container').height()});
+
+      // Back button
+      $("#map-control-secondary-header i").click(function(e){
+        $("#map-control-header").removeClass('second-page');
+        $("#map-control-container").css({'max-height':''}).removeClass('second-page');
+      });
     });
   }
 
